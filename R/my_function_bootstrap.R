8cea68a934a36f0a6cd8b2c2aaab3e7570c3558f
Griffith Xu <102710723+YXu120@users.noreply.github.com>
2022-12-13 17:51:11 -0500

    Dec 13.

--- R/my_function_bootstrap.R
+++ R/my_function_bootstrap.R
@@ -1,7 +1,7 @@
 #' Final Project Parametric Bootstrap
 #'
 #' @param data the data set for the example
-#' @param example the name of example: one of "culcita", "ctsib", "epilepsy", or "tortoise"
+#' @param example the name of example, in our case we focus on "tortoise"
 #' @param B sample size of bootstrapping
 #'
 #' @return A list with summary statistics including point estimates of coefficients, standard errors, confidence intervals and p-value for hypothesis test.
@@ -21,15 +21,45 @@ para_boot <- function(data, example, B){
   ## Number of rows of data
   n <- nrow(data)
 
-  ## Obtain the fitted values of coefficients, and sigma by using run_model function
+  ## Obtain the results we will use after fitting the model from run_model function
   beta_0 <- run_model(tortoise, "tortoise")[[1]][[1]]
   beta_1 <- run_model(tortoise, "tortoise")[[1]][[2]]
+  year_factor <- c(0, run_model(tortoise, "tortoise")[[1]][[3]], run_model(tortoise, "tortoise")[[1]][[4]])
   sigma <- sqrare(run_model(tortoise, "tortoise")[[2]])
 
-  ## Generate random effect Z* from N(0, sigma^2)
-  bootstrap_Z <- tibble(B = 1:B) %>%
+  ## The steps of parametric bootstrap are as follows:
+  ## 1. Generate random effect Z* from N(0, sigma^2)
+  Z <- rnorm(n, mean = 0, sd = sigma)
+  Z <- rep(Z, each = 3)
+
+  ## 2. Compute mu_hat = exp(beta_0 + log(x_area) + year_factor + beta_1 * x_prev + Z
+  # x_area
+  x_area <- data %>%
+    select(Area)
+  # x_prev
+  x_prev <- data %>%
+    select(prev)
+  # year_factor
+  year_factor <- rep(year_factor , times = 10)
+  # mu_hat
+  mu_hat <- exp(beta_0 + log(x_area) + year_factor + beta_1 * x_prev + Z)
+
+  ## 3. Generate B data (Y_1, ..., Y_B) from Poisson(mu_hat)
+  Y_btsp <- tibble(B = 1:B) %>%
     group_by(B) %>%
-    summarize(d = rnorm(n, mean = 0, sd = sigma), .groups = "keep")
+    summarize(Y_hat = rpois(30, mu_hat[1:30,]), .groups = "drop") %>%
+    nest_by(B) %>%
+    summarize(glmer(Y_hat~prev+offset(log(Area))+factor(year)+(1|Site),
+                   family=poisson,
+                   data=data), .groups = "drop")
+
+
+
+
+
+  Y_btsp
+
+
 
   ## Bootstrap standard deviation
   sd(bootstrap1$lambda_hat)
