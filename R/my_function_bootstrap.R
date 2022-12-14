ecb1822dd2627d9f344f44586943978bb52be836
Griffith Xu <102710723+YXu120@users.noreply.github.com>
2022-12-13 22:10:50 -0500

    Dec 13.

--- R/my_function_bootstrap.R
+++ R/my_function_bootstrap.R
@@ -25,7 +25,7 @@ para_boot <- function(data, example, B){
   beta_0 <- run_model(tortoise, "tortoise")[[1]][[1]]
   beta_1 <- run_model(tortoise, "tortoise")[[1]][[2]]
   year_factor <- c(0, run_model(tortoise, "tortoise")[[1]][[3]], run_model(tortoise, "tortoise")[[1]][[4]])
-  sigma <- sqrare(run_model(tortoise, "tortoise")[[2]])
+  sigma <- sqrt(run_model(tortoise, "tortoise")[[2]])
 
   ## The steps of parametric bootstrap are as follows:
   ## 1. Generate random effect Z* from N(0, sigma^2)
@@ -44,29 +44,32 @@ para_boot <- function(data, example, B){
   # mu_hat
   mu_hat <- exp(beta_0 + log(x_area) + year_factor + beta_1 * x_prev + Z)
 
-  ## 3. Generate B data (Y_1, ..., Y_B) from Poisson(mu_hat)
+  ## 3. Resample (Y_1, ..., Y_B) from Poisson(mu_hat) for each i, j
   Y_btsp <- tibble(B = 1:B) %>%
+    crossing(data) %>%
     group_by(B) %>%
-    summarize(Y_hat = rpois(30, mu_hat[1:30,]), .groups = "drop") %>%
-    nest_by(B) %>%
-    summarize(glmer(Y_hat~prev+offset(log(Area))+factor(year)+(1|Site),
-                   family=poisson,
-                   data=data), .groups = "drop")
+    summarize(shells = rpois(30, mu_hat[1:30,]), .groups = "drop") %>%
+    nest_by(B)
 
-
-
-
-
-  Y_btsp
-
-
+  ## 4. Refit with the sampled (Y_1, ..., Y_B) to approximate betas
+  beta_1_star <- array(data = NA, dim = B)
+  for(i in 1:B){
+    data$shells <- unlist(Y_btsp$data[[i]])
+    beta_1_star[i] <- run_model(data, "tortoise")[[1]][[2]]
+  }
 
   ## Bootstrap standard deviation
-  sd(bootstrap1$lambda_hat)
+  beta_sd <- sd(beta_1_star)
 
-  ## Bootstrap confidence interval
-  quantile(bootstrap1$lambda_hat,c(.025,.975))
+  ## Bootstrap 95% confidence interval
+  beta_ci <- quantile(beta_1_star,c(.025,.975))
 
-  ## Bootstrap bias
-  mean(bootstrap1$lambda_hat) - lambda_hat
+  ## Test statistics and p-values
+
+
+  ## Return values
+  list(point_estimation = beta_1,
+       standard_deviation = beta_sd,
+       `95%_confidence_interval` = beta_ci,
+       p_value = test_stat)
 }
