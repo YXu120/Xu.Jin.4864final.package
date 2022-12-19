52ffde287f2040712992d3a60fb90db6a4dabd1e
Griffith Xu <102710723+YXu120@users.noreply.github.com>
2022-12-18 21:45:28 -0500

    p-value.

--- R/my_function_bootstrap.R
+++ R/my_function_bootstrap.R
@@ -28,6 +28,7 @@ para_boot <- function(data, example, B){
   beta_1 <- model_summary[[1]][[2]]
   year_factor <- c(0, model_summary[[1]][[3]], model_summary[[1]][[4]])
   sigma <- sqrt(model_summary[[2]])
+  test_stats <- model_summary[[4]][[2]]
 
   ## The steps of parametric bootstrap are as follows:
   ## 1. Generate random effect Z* from N(0, sigma^2)
@@ -70,8 +71,28 @@ para_boot <- function(data, example, B){
   # Bootstrap 95% confidence interval
   beta_ci <- quantile(beta_btsp$beta_1_star,c(.025,.975))
 
-  # P-value for hypothesis test, where H0: beta_1 = 0 and alpha = 0.05
-  p <- mean(qnorm(0.975) > abs(beta_btsp$test_statistic))
+  ## * P-value for hypothesis test, where H0: beta_1 = 0
+  # mu_hat_test, under H0, beta_1 = 0
+  mu_hat_test <- exp(beta_0 + log(x_area) + year_factor + Z)
+  # Second bootstrap
+  Y_btsp_test <- tibble(B = 1:B) %>%
+    crossing(data) %>%
+    group_by(B) %>%
+    mutate(shells = rpois(n, mu_hat_test[1:n,]))
+
+  Y_btsp_test <- tibble(B = 1:B) %>%
+    crossing(data) %>%
+    mutate(shells = Y_btsp_test$shells) %>%
+    nest_by(B)
+
+  beta_btsp_test <- tibble(t = 1:B) %>%
+    group_by(t) %>%
+    summarize(mod_sum = suppressMessages(run_model(Y_btsp_test$data[[t]], example)), .groups = "keep") %>%
+    summarize(beta_1_star = mod_sum[[1]][[2]],
+              test_statistic = mod_sum[[4]][[2]], .groups = "drop")
+
+  # Compute p-value
+  p <- mean(abs(test_stats) < abs(beta_btsp_test$test_statistic))
   if (p == 0) {
     p <- "<.001"
   }
